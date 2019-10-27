bbd16b8207265cdc239d44d254a7779de6f20a2f
Elijah Lee <develijahlee@gmail.com>
2019-10-27 18:40:15 +0900

    Updates

---
+++ precache-manifest.46bbe370d42ae7c226a47a46ed7ba3bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c319c54c6bfd8e5145678a00294e875e",
+    "url": "/andair/index.html"
+  },
+  {
+    "revision": "ddf35218db027f0c1aae",
+    "url": "/andair/static/css/main.3d7430c8.chunk.css"
+  },
+  {
+    "revision": "afee38cb3dd4fef0253b",
+    "url": "/andair/static/js/2.b76c0769.chunk.js"
+  },
+  {
+    "revision": "ddf35218db027f0c1aae",
+    "url": "/andair/static/js/main.6c3fe2fe.chunk.js"
+  },
+  {
+    "revision": "3e9fb12b2b6893fb98e8",
+    "url": "/andair/static/js/runtime-main.3e141470.js"
+  }
+]);
